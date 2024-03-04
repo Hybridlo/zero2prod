c55292a209570a0764f97c9baef9b6f2e6119427
Павел <pashanoy@gmail.com>
2024-03-04 12:50:58 +0200

    More tests, add timeouts to http requests

--- src/email_client.rs
+++ src/email_client.rs
@@ -12,9 +12,14 @@ pub struct EmailClient {
 }
 
 impl EmailClient {
-    pub fn new(base_url: String, sender: SubscriberEmail, authorization_token: Secret<String>) -> Self {
+    pub fn new(
+        base_url: String,
+        sender: SubscriberEmail,
+        authorization_token: Secret<String>,
+        timeout: std::time::Duration,
+    ) -> Self {
         Self {
-            http_client: Client::new(),
+            http_client: Client::builder().timeout(timeout).build().unwrap(),
             base_url,
             sender,
             authorization_token,
@@ -39,7 +44,12 @@ impl EmailClient {
             body_text: text_content,
             is_transactional,
         };
-        self.http_client.post(&url).json(&request_body).send().await?;
+        self.http_client
+            .post(&url)
+            .json(&request_body)
+            .send()
+            .await?
+            .error_for_status()?;
         Ok(())
     }
 }
@@ -58,9 +68,19 @@ struct SendEmailRequest<'a> {
 
 #[cfg(test)]
 mod tests {
-    use fake::{faker::{internet::en::SafeEmail, lorem::en::{Paragraph, Sentence}}, Fake, Faker};
+    use claim::{assert_err, assert_ok};
+    use fake::{
+        faker::{
+            internet::en::SafeEmail,
+            lorem::en::{Paragraph, Sentence},
+        },
+        Fake, Faker,
+    };
     use secrecy::Secret;
-    use wiremock::{matchers::{header, method, path}, Mock, MockServer, ResponseTemplate};
+    use wiremock::{
+        matchers::{any, header, method, path},
+        Mock, MockServer, ResponseTemplate,
+    };
 
     use crate::{domain::SubscriberEmail, email_client::EmailClient};
 
@@ -84,6 +104,31 @@ mod tests {
         }
     }
 
+    /// Generate a random email subject
+    fn subject() -> String {
+        Sentence(1..2).fake()
+    }
+
+    /// Generate a random email content
+    fn content() -> String {
+        Paragraph(1..10).fake()
+    }
+
+    /// Generate a random subscriber email
+    fn email() -> SubscriberEmail {
+        SubscriberEmail::parse(SafeEmail().fake()).unwrap()
+    }
+
+    /// Get a test instance of `EmailClient`
+    fn email_client(base_url: String) -> EmailClient {
+        EmailClient::new(
+            base_url,
+            email(),
+            Secret::new(Faker.fake()),
+            std::time::Duration::from_millis(200),
+        )
+    }
+
     #[tokio::test]
     async fn send_email_fires_a_request_to_base_url() {
         // Arrange
@@ -91,8 +136,7 @@ mod tests {
         // not the fact that you're sending an HTTP request, it should be obvious from the code;
         // It's probably a valid use-case for an integration test tho.
         let mock_server = MockServer::start().await;
-        let sender = SubscriberEmail::parse(SafeEmail().fake()).unwrap();
-        let email_client = EmailClient::new(mock_server.uri(), sender, Secret::new(Faker.fake()));
+        let email_client = email_client(mock_server.uri());
 
         Mock::given(SendEmailBodyMatcher)
             .and(header("Content-Type", "application/json"))
@@ -103,16 +147,75 @@ mod tests {
             .mount(&mock_server)
             .await;
 
-        let subscriber_email = SubscriberEmail::parse(SafeEmail().fake()).unwrap();
-        let subject: String = Sentence(1..2).fake();
-        let content: String = Paragraph(1..10).fake();
-
         // Act
         let _ = email_client
-            .send_email(subscriber_email, &subject, &content, &content, true)
+            .send_email(email(), &subject(), &content(), &content(), true)
             .await;
 
         // Assert
         // Mock expectations are checked on drop
+    }
+
+    #[tokio::test]
+    async fn send_email_succeeds_if_the_server_returns_200() {
+        // Arrange
+        let mock_server = MockServer::start().await;
+        let email_client = email_client(mock_server.uri());
+
+        Mock::given(any())
+            .respond_with(ResponseTemplate::new(200))
+            .expect(1)
+            .mount(&mock_server)
+            .await;
+
+        // Act
+        let outcome = email_client
+            .send_email(email(), &subject(), &content(), &content(), true)
+            .await;
+
+        // Assert
+        assert_ok!(outcome);
+    }
+
+    #[tokio::test]
+    async fn send_email_fails_if_the_server_returns_500() {
+        // Arrange
+        let mock_server = MockServer::start().await;
+        let email_client = email_client(mock_server.uri());
+
+        Mock::given(any())
+            .respond_with(ResponseTemplate::new(500))
+            .expect(1)
+            .mount(&mock_server)
+            .await;
+
+        // Act
+        let outcome = email_client
+            .send_email(email(), &subject(), &content(), &content(), true)
+            .await;
+
+        // Assert
+        assert_err!(outcome);
+    }
+
+    #[tokio::test]
+    async fn send_email_times_out_if_the_server_takes_too_long() {
+        // Arrange
+        let mock_server = MockServer::start().await;
+        let email_client = email_client(mock_server.uri());
+
+        Mock::given(any())
+            .respond_with(ResponseTemplate::new(200).set_delay(std::time::Duration::from_secs(180)))
+            .expect(1)
+            .mount(&mock_server)
+            .await;
+
+        // Act
+        let outcome = email_client
+            .send_email(email(), &subject(), &content(), &content(), true)
+            .await;
+
+        // Assert
+        assert_err!(outcome);
     }
 }
--- src/startup.rs
+++ src/startup.rs
@@ -4,9 +4,16 @@ use actix_web::{dev::Server, web, App, HttpServer};
 use sqlx::PgPool;
 use tracing_actix_web::TracingLogger;
 
-use crate::{email_client::EmailClient, routes::{health_check, subscribe}};
+use crate::{
+    email_client::EmailClient,
+    routes::{health_check, subscribe},
+};
 
-pub fn run(listener: TcpListener, db_pool: PgPool, email_client: EmailClient) -> Result<Server, std::io::Error> {
+pub fn run(
+    listener: TcpListener,
+    db_pool: PgPool,
+    email_client: EmailClient,
+) -> Result<Server, std::io::Error> {
     let db_pool = web::Data::new(db_pool);
     let email_client = Arc::new(email_client);
     let server = HttpServer::new(move || {
