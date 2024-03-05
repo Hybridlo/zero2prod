b3ef0b9623cd7a701c3df011194be0075070cdb8
Павел <pashanoy@gmail.com>
2024-03-05 15:29:39 +0200

    Store passwords hash instead of plain text password

--- src/routes/newsletter.rs
+++ src/routes/newsletter.rs
@@ -7,6 +7,7 @@ use base64::Engine;
 use reqwest::{header::HeaderValue, StatusCode};
 use secrecy::{ExposeSecret as _, Secret};
 use serde::Deserialize;
+use sha3::Digest as _;
 use sqlx::PgPool;
 
 use crate::{domain::SubscriberEmail, email_client::EmailClient};
@@ -109,14 +110,16 @@ async fn validate_credentials(
     credentials: Credentials,
     pool: &PgPool,
 ) -> Result<uuid::Uuid, PublishError> {
+    let password_hash = sha3::Sha3_256::digest(credentials.password.expose_secret().as_bytes());
+    let password_hash = format!("{:x}", password_hash);
     let user_id = sqlx::query!(
         r#"
         SELECT user_id
         FROM users
-        WHERE username = $1 AND password = $2
+        WHERE username = $1 AND password_hash = $2
         "#,
         credentials.username,
-        credentials.password.expose_secret()
+        password_hash
     )
     .fetch_optional(pool)
     .await
